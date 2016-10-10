4bb691a7ac01abe84881b19c8c71eda5ec9d5b86
Theo <tmarkovic@ttmedia.se>
2016-10-10 19:30:00 +0200

    men vadfan

---
+++ src/vendor.ts
@@ -0,0 +1,14 @@
+// Angular Deps
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+import '@angular/forms';
+
+// RxJS
+import 'rxjs';
+
+import 'bulma/css/bulma.css';
+import 'font-awesome/css/font-awesome.min.css';
