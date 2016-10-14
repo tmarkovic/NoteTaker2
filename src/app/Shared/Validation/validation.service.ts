0850f314bc5bec53c81868644f723db0f1f4445b
Theo <tmarkovic@ttmedia.se>
2016-10-14 16:02:08 +0200

    fixed shit

--- src/app/Shared/Validation/validation.service.ts
+++ src/app/Shared/Validation/validation.service.ts
@@ -4,14 +4,14 @@ import { Injectable } from '@angular/core';
 export class ValidationService {
 
 
-    static getValidationError(validatorError: string, validatorValue?: any, controlName?: string): string {
+    static getValidationError(validatorError: string, validatorValue?: any): string {
         let validationMessage: Object = {
             'minLength': `must be longer than ${validatorValue.requiredLength}`,
             'required': 'cannot be empty',
             'passwordMatch': 'does not match'
         }
 
-        return `${controlName ? controlName : 'Entered value'} ${validationMessage[validatorError]}`;
+        return `${validationMessage[validatorError]}`;
     }
     constructor() { }
 }
